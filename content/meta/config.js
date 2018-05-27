19b77e37d7fbfa382e6d71c25277f10a4d9efabc
Thiago Santana <thiago.santana@gmail.com>
2018-05-26 23:39:05 -0300

    first commit

---
+++ content/meta/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  siteTitle: "Thiago Santana - Desenvolvimento de Software", // <title>
+  shortSiteTitle: "Thiago Santana - Desenvolvimento de Software", // <title> ending for posts and pages
+  siteDescription: "Desenvolvimento de software em geral.",
+  siteUrl: "https://thiagosantana.online",
+  pathPrefix: "",
+  siteImage: "preview.jpg",
+  siteLanguage: "pt-BR",
+  // author
+  authorName: "Thiago Santana",
+  authorTwitterAccount: "thiagosantana",
+  // info
+  headerTitle: "thiago santana",
+  headerSubTitle: "desenvolvimento de software",
+  // manifest.json
+  manifestName: "Thiago Santana - Desenvolvimento de Software",
+  manifestShortName: "ThiSantana", // max 12 characters
+  manifestStartUrl: "/index.html",
+  manifestBackgroundColor: "white",
+  manifestThemeColor: "#666",
+  manifestDisplay: "standalone",
+  // social
+  authorSocialLinks: [
+    { name: "github", url: "https://github.com/greglobinski" },
+    { name: "twitter", url: "https://twitter.com/greglobinski" },
+    { name: "facebook", url: "http://facebook.com/greglobinski" }
+  ]
+};
